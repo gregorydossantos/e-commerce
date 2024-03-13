689f0515c801ec1e2014d875e912fd55c454982d
Gregory Castezana <gregory.castezana@ibm.com>
2024-03-13 00:16:25 -0300

    Implementing ShopCartResponse class

--- shop-cart/src/main/java/com/fiap/gregory/shopcart/rest/dto/response/ShopCartResponse.java
+++ shop-cart/src/main/java/com/fiap/gregory/shopcart/rest/dto/response/ShopCartResponse.java
@@ -1,4 +1,27 @@
 package com.fiap.gregory.shopcart.rest.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fiap.gregory.client.infra.db.model.Client;
+import com.fiap.gregory.itemmanager.rest.dto.response.ItemResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.WRITE_ONLY;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShopCartResponse {
+
+    @JsonProperty(access = WRITE_ONLY)
+    private Long id;
+    private Integer amount;
+    private BigDecimal total;
+    private Client client;
+    private ItemResponse item;
 }

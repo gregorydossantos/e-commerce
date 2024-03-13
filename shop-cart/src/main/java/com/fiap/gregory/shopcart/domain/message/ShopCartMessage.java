fa356ffc3960eb42f22fa92ccbffb6a198c2185a
Gregory Castezana <gregory.castezana@ibm.com>
2024-03-13 00:08:39 -0300

    Implementing ShopCartRequest class and add commons messages

--- shop-cart/src/main/java/com/fiap/gregory/shopcart/domain/message/ShopCartMessage.java
+++ shop-cart/src/main/java/com/fiap/gregory/shopcart/domain/message/ShopCartMessage.java
@@ -7,4 +7,6 @@ public class ShopCartMessage {
     public static final String DATA_INTEGRITY = "Shopping cart already register!";
     public static final String SHOP_CART_NOT_FOUND = "Shopping cart not found!";
     public static final String FIELD_MANDATORY = "Is a mandatory field!";
+    public static final String INVALID_PRICE = "Invalid price receive from request!";
+    public static final String INVALID_AMOUNT = "Invalid amount receive from request!";
 }
--- shop-cart/src/main/java/com/fiap/gregory/shopcart/rest/dto/request/ShopCartRequest.java
+++ shop-cart/src/main/java/com/fiap/gregory/shopcart/rest/dto/request/ShopCartRequest.java
@@ -1,5 +1,7 @@
 package com.fiap.gregory.shopcart.rest.dto.request;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -7,7 +9,7 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 
-import static com.fiap.gregory.shopcart.domain.message.ShopCartMessage.FIELD_MANDATORY;
+import static com.fiap.gregory.shopcart.domain.message.ShopCartMessage.*;
 
 @Data
 @Builder
@@ -15,8 +17,12 @@ import static com.fiap.gregory.shopcart.domain.message.ShopCartMessage.FIELD_MAN
 @NotEmpty(message = FIELD_MANDATORY)
 public class ShopCartRequest {
 
+    @Min(value = 1, message = INVALID_AMOUNT)
     private Integer amount;
+
+    @Digits(integer = 10, fraction = 2, message = INVALID_PRICE)
     private BigDecimal total;
+
     private Long clientId;
     private Long itemId;
 }

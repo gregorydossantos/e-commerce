5248e28ad94f97493aeb2961330e595322b04043
Gregory Castezana <gregory.castezana@ibm.com>
2024-03-19 16:06:44 -0300

    Implements infra db layer

--- payment/src/main/java/com/fiap/gregory/payment/infra/db/model/Payment.java
+++ payment/src/main/java/com/fiap/gregory/payment/infra/db/model/Payment.java
@@ -1,4 +1,29 @@
 package com.fiap.gregory.payment.infra.db.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "tb_payment")
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "transaction", length = 1)
+    String transaction;
+
+    @Column(name = "paid")
+    Boolean paid;
+
+    @Column(name = "id_shop_cart")
+    Long idShopCart;
 }

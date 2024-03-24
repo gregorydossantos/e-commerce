6923f5268e84b42da3361710d853169e3d107b83
Gregory Castezana <gregory.castezana@ibm.com>
2024-03-24 01:15:36 -0300

    Implements unit tests in Payment module

---
+++ payment/src/test/java/com/fiap/gregory/payment/external/api/shoppingcart/rest/impl/ShopCartRestTemplateImplTest.java
@@ -0,0 +1,14 @@
+package com.fiap.gregory.payment.external.api.shoppingcart.rest.impl;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopCartRestTemplateImplTest {
+
+    @Test
+    @DisplayName("EXTERNAL API LAYER ::: Call Shopping Cart API")
+    void getShoppingCartById() {
+    }
+}
